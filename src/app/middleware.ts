b175696980cfa2290416cf4529a04d19cdb6563a
Jeremiah Adeboye <91225691+jsCoder07@users.noreply.github.com>
2024-07-17 04:50:53 +0100

    Add files via upload

--- src/app/middleware.ts
+++ src/app/middleware.ts
@@ -1,12 +1,6 @@
-import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
-// Create Protected Routes
-const isProtected=createRouteMatcher(["/settings(.*)"]) 
-export default clerkMiddleware((auth,req)=>{
-    // Check if the requested page is protected and protect if user isnt authenticated
-    if(isProtected(req)){
-        auth().protect();
-    }
-});
+import { clerkMiddleware } from "@clerk/nextjs/server";
+
+export default clerkMiddleware();
 
 export const config = {
   matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
